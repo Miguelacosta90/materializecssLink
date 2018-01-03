8ef57c5ddc8896106bbe295112a1447852d675ff
miguel acosta <miguelacosta@miguels-MacBook-Pro.local>
2018-01-03 11:00:40 -0800

    real first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+$("document").ready(function() {
+  $("#text").append("<p>this line is for your JavaScript making sure your page is linked</p>");
+  $("#name").append("<h1>Miguel Acosta</h1>");
+});
